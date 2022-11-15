86fdaef7d1b87ac70ff17cd5afcc095bb7710880
D.N.Singh <76054685+DNSingh-15@users.noreply.github.com>
2022-11-15 18:11:20 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const zmq = require("zeromq")
+
+async function run() {
+    const sock = new zmq.Push
+
+    await sock.bind("tcp://127.0.0.1:3000")
+    console.log("Producer bound to port 3000")
+
+    while (true) {
+        await sock.send("some work")
+        await new Promise(resolve => setTimeout(resolve, 1000))
+    }
+}
+
+run()
+
